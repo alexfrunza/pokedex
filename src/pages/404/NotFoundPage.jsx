b8eae983d301110ce7e37c7d9d616e4a621387eb
Alexandru Frunza <frunza.alexandru.2004@gmail.com>
2022-03-15 17:45:01 +0200

    Add 404 page

--- src/pages/404/NotFoundPage.jsx
+++ src/pages/404/NotFoundPage.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import "./NotFoundPage.css";
+import notFoundImage from "images/404.png";
+import { Link } from 'react-router-dom';
 
 export default function NotFoundPage() {
-    return <div> Not Found </div>;
+    return (
+        <div className='not-found'>
+            <img src={notFoundImage} alt="pokeball" />
+            <div className='message'>
+                <h2> Uh-oh! </h2>
+                <p> You look lost on your journey! </p>
+                <Link to='/' className='go-back-button'> Go Back Home </Link>
+            </div>
+        </div>
+    );
 }
