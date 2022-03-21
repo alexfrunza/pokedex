e410b56ae869e7c4a68a6282ab7198833eeca786
Alexandru Frunza <frunza.alexandru.2004@gmail.com>
2022-03-21 18:11:09 +0200

    Try to reformat graph

--- src/components/StatsChart/StatsChart.jsx
+++ src/components/StatsChart/StatsChart.jsx
@@ -1,32 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./StatsChart.css";
 import { toTitleCase } from "shared/helpers.js";
 import { nanoid } from "nanoid";
 
 export default function StatsChart({ stats }) {
-    function renderColumn(points) {
-        const filledBoxes = Math.ceil(points / 17);
+    const [columns, setColumns] = useState([]);
+
+    function generateBoxes() {
         const boxes = [];
-
-        // Add empty boxes to array
-        for (let i = 1; i <= 15 - filledBoxes; i++) {
-            boxes.push(<li key={nanoid()} className="empty-box"></li>);
-        }
-
-        // Add filled boxes to array
-        for (let i = 1; i <= filledBoxes; i++) {
-            boxes.push(<li key={nanoid()} className="filled-box"></li>);
+        for (let i = 0; i < 15; i++) {
+            boxes.push({ id: nanoid() });
         }
         return boxes;
     }
 
+    function colorBoxes(boxes, points) {
+        const filledBoxes = Math.ceil(points / 17);
+
+        return boxes.map((box, index) => {
+            return {
+                ...box,
+                className:
+                    index < 15 - filledBoxes ? "empty-box" : "filled-box",
+            };
+        });
+    }
+
+    useEffect(() => {
+        setColumns(
+            stats.map(({ stat: { name } }) => {
+                return {
+                    name,
+                    boxes: generateBoxes(),
+                };
+            })
+        );
+        // eslint-disable-next-line
+    }, []);
+
+    useEffect(() => {
+        setColumns(
+            stats.map(({ base_stat, stat: { name } }, index) => {
+                return {
+                    name,
+                    boxes: colorBoxes(columns[index].boxes, base_stat),
+                };
+            })
+        );
+        // eslint-disable-next-line
+    }, [stats]);
+
+    function renderColumnElements(boxes) {
+        return boxes.map(({ id, className }) => {
+            return <li key={id} className={className}></li>;
+        });
+    }
+
     return (
         <div className="stats-chart">
-            <p className='title'> Stats </p>
-            {stats.map(({ base_stat, stat: { name } }) => {
+            <p className="title"> Stats </p>
+            {columns.map(({ name, boxes }) => {
                 return (
-                    <ul key={nanoid()}>
-                        {renderColumn(base_stat)}
+                    <ul key={`${name}`}>
+                        {renderColumnElements(boxes)}
                         {toTitleCase(name.replaceAll("-", " "))}
                     </ul>
                 );
